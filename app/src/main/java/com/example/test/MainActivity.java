78e1f86964de0bce34f415d4e252a348dbad0f3b
VianneyLeblanc <vianney.leblanc@etu.univ-smb.fr>
2019-03-11 15:09:05 +0100

    navigation menu

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startActivity(new Intent(MainActivity.this,GameActivity.class));
+                startActivity(new Intent(MainActivity.this,Game.class));
             }
         });
     }
