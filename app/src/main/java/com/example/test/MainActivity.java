8032d3944037bcaf3c48b4a6f770aa456d907841
johan <gruazjoh@iut-acy.univ-smb.fr>
2019-03-18 14:42:01 +0100

    okhttp

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,9 +3,24 @@ package com.example.test;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    public MainActivity() throws IOException {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +29,17 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                startActivity(new Intent(MainActivity.this,GameActivity.class));
+                /*ObtenirUrl example = new ObtenirUrl();
+                String response = null;
+                try {
+                    response = example.run("https://rickandmortyapi.com/api/character");
+                    Log.i("tag response", "onClick: "+response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                new ObtenirUrl().execute();
+
+                //startActivity(new Intent(MainActivity.this,Game.class));
             }
         });
     }
